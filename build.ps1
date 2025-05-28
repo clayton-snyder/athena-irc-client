c533e5f0328cc8569dfc77961ef66c834970c145
Clayton <snyderc@gmx.com>
2025-05-28 15:01:18 -0700

    Creates a separate terminalutils library for text coloring/formatting and changes log.c to use that.

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
 # Must be run from a Developer PS/cmd session. Run Launch-VsDevShell.ps1 first.
-cl main.c msgqueue.c log.c ws2_32.lib /DWIN32_LEAN_AND_MEAN /Fe: client.exe
+cl main.c msgqueue.c log.c terminalutils.c ws2_32.lib /DWIN32_LEAN_AND_MEAN /DTERMUTILS_DEBUG_ASSERT /Fe: client.exe
