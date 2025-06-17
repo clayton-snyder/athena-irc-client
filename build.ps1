b24333af3fb594f4ae8d08694294437d184894cb
Clayton <snyderc@gmx.com>
2025-06-17 01:35:42 -0700

    Reorganize files and update build script

--- build.ps1
+++ build.ps1
@@ -5,16 +5,18 @@ param (
     [switch] $LeaveOldArtifacts
 )
 
-$outfilename = "client.exe";
+$output_dir = "built"
+$exe_filename = "client.exe";
+
+if (-not(test-path "$output_dir")) {
+    mkdir "$output_dir";
+}
 
 if ($LeaveOldArtifacts) {
     Write-Host "LeaveOldArtifacts enabled; not clearing old output files first." -Foreground Yellow
 } else {
-    rm *.obj;
-    # Avoids accidentally running an old build after a failure.
-    if (test-path "$outfilename") {
-        rm "$outfilename";
-    }
+    rm "$output_dir\*.obj";
+    rm "$output_dir\*.exe";
 }
 
 $warnings_as_errors = "/WX";
@@ -27,4 +29,6 @@ if ($AllowWarnings) {
 # Disabled warnings:
 # * [4820] Padding inserted after data member (winsock2.h causes these).
 # * [5045] Notes where compiler may insert Qspectre protection (potential perf cost).
-cl main.c msgqueue.c log.c terminalutils.c stringutils.c screen_framework.c msgutils.c handlers.c ws2_32.lib /Qspectre /DWIN32_LEAN_AND_MEAN /DTERMUTILS_DEBUG_ASSERT /Wall /wd4820 /wd5045 $warnings_as_errors /Fe: "$outfilename";
+#cl main.c msgqueue.c log.c terminalutils.c stringutils.c screen_framework.c msgutils.c handlers.c ws2_32.lib /Qspectre /DWIN32_LEAN_AND_MEAN /DTERMUTILS_DEBUG_ASSERT /Wall /wd4820 /wd5045 $warnings_as_errors /Fe: "$outfilename";
+cl src\*.c ws2_32.lib /I"include" /Qspectre /DWIN32_LEAN_AND_MEAN /DTERMUTILS_DEBUG_ASSERT /Wall /wd4820 /wd5045 $warnings_as_errors /Fo"$output_dir\" /Fe"$output_dir\$exe_filename";
+#/link /out:"$output_dir\$exe_filename";
